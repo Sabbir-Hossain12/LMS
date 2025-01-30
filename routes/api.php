d6d903c81c35ad072475593779a330c573a2912d
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2025-01-30 18:04:16 +0600

    sanctum installed, API development begins

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('apiv2')->group(function () {
+Route::get('/test', function () {
+    return "hello";
+});
+});
