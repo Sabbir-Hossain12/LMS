2d6003e51ec6074b1900e6ab7fba06a079333035
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2026-01-07 17:13:12 +0600

    Lesson youtube  video covered top and buttom

--- resources/views/Frontend/pages/lesson/include/video.blade.php
+++ resources/views/Frontend/pages/lesson/include/video.blade.php
@@ -1,19 +1,30 @@
 <style>
-    .lesson__content__wrap
-    {
+    .lesson__content__wrap {
         padding: 15px 20px !important;
     }
-    
+
     @media (max-width: 767px) {
-    /* CSS for mobile devices */
-    .lesson__content__wrap {
-         padding: 12px 20px !important;
+        /* CSS for mobile devices */
+        .lesson__content__wrap {
+            padding: 12px 20px !important;
+        }
     }
-}
+
+    .lesson__content__wrap.footer {
+        padding: 15px 10px !important;
+        bottom: 0 !important;
+    }
+
+    @media (min-width: 767px) {
+        .lesson__content__wrap.footer {
+            padding: 23px 10px !important;
+            bottom: 0 !important;
+        }
+    }
 </style>
 <div class="lesson__content__main">
     @if($video->start_time <= now())
-        
+
         <div class="lesson__content__wrap">
             <h3>{{$video->title ?? ''}}</h3>
             {{--        <span><a href="javascript:void(0)">Close</a></span>--}}
@@ -22,12 +33,12 @@
         <!--<div class="plyr__video-embed rbtplayer">-->
         <!--    <iframe src="{{$video->video_url ?? ''}}" allowfullscreen="" allow="autoplay"></iframe>-->
         <!--</div>-->
-        
-         @if($video->video_file) 
+
+        @if($video->video_file)
             {{-- Play uploaded video --}}
             <video class="rbtplayer" id="lessonVideoPlayer" controls playsinline
-                 oncontextmenu="return false;"  
-                controlsList="nodownload noremoteplayback noplaybackrate" width="100%">
+                   oncontextmenu="return false;"
+                   controlsList="nodownload noremoteplayback noplaybackrate" width="100%">
                 <source src="{{ asset($video->video_file) }}" type="video/mp4">
                 <!--Your browser does not support the video tag.-->
             </video>
@@ -38,12 +49,14 @@
             </div>
         @endif
 
- 
-
     @else
         <h3 class="text-center mt-4">Video will be available
             on {{date('d M, Y h:i A', strtotime($video->start_time))}}</h3>
     @endif
+
+    <div class="lesson__content__wrap footer">
+        <h3></h3>
+        {{--        <span><a href="javascript:void(0)">Close</a></span>--}}
+    </div>
 </div>
 
-           
